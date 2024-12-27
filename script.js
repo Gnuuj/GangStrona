cebc439384acb78966e95ff91006b3f7f245b26c
Gnuuj <106268395+Gnuuj@users.noreply.github.com>
2024-12-27 16:05:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function sendMail() {
     let parms = {
         email: document.getElementById("email").value,
-        message : document.getElementById("message").value
+        subject: document.getElementById("subject").value,
+        message : document.getElementById("message").value,
     }
 
     emailjs.send("service_gfzta6n", "template_nksjuze", parms).then(alert("Email sent successfully!"));
